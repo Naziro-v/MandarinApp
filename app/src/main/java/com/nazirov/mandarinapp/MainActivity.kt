551eacd2f52114e7dabf441456610c14a73cc175
Nazirov <nazirovelmurod6@gmail.com>
2022-01-19 17:25:31 +0500

    first commit

---
+++ app/src/main/java/com/nazirov/mandarinapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.nazirov.mandarinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var pages: ArrayList<Page>
+    private lateinit var mainPagerAdapter: MainPagerAdapter
+    private lateinit var mainTab : TabLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        pages = ArrayList()
+        pages = addPages()
+        initViews()
+
+
+
+    }
+
+    private fun addPages(): ArrayList<Page> {
+
+        return ArrayList<Page>().apply {
+            for (i in 0..2) {
+                this.add(
+                    Page(
+                        "Hi",
+                        "Mirkamol"
+                    )
+                )
+            }
+        }
+    }
+
+    private fun initViews() {
+        viewPager = findViewById(R.id.vp_main)
+        mainTab = findViewById(R.id.tab_main)
+        mainPagerAdapter = MainPagerAdapter(pages, this)
+        viewPager.adapter = mainPagerAdapter
+
+        mainTab.setupWithViewPager(viewPager)
+    }
+}
---
+++ app/src/main/java/com/nazirov/mandarinapp/Page.kt
@@ -0,0 +1,9 @@
+package com.nazirov.mandarinapp
+
+import android.icu.text.CaseMap
+
+data class Page(
+
+    val title: String,
+    val description : String,
+)
