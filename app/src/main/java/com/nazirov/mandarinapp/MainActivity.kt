87f6fb3815c51d5f56c6f2a6c01dede131461adc
Nazirov <nazirovelmurod6@gmail.com>
2022-02-02 09:05:37 +0500

    .

--- app/src/main/java/com/nazirov/mandarinapp/MainActivity.kt
+++ app/src/main/java/com/nazirov/mandarinapp/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
                 this.add(
                     Page(
                         "Hi",
-                        "Mirkamol"
+                        "Elmurod"
                     )
                 )
             }
